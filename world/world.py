934655629e92b1eeacf29f77884329e713659015
Ain Soph Aur <ainsoph_itmo@mail.ru>
2026-06-02 17:58:24 +0300

    Finished moves and winning logic

--- world/world.py
+++ world/world.py
@@ -1,9 +1,9 @@
 import numpy as np
-from typing import *
+from typing import Optional, List, Tuple, Literal
+
 
 class TicTacToeEnvironment:
-    
-    def __init__(self, field: Optional[List] = None) -> None:
+    def __init__(self, field: Optional[List[int]] = None) -> None:
         """Initializes the environment
 
         Args:
@@ -12,59 +12,109 @@ class TicTacToeEnvironment:
         Raises:
             ValueError: if field argument is of inappropriate size
         """
-        self.board: np.ndarray = np.zeros((3,3)).astype(int)
-        self.turn: int = 1 # X moves first by default
+        self.board: np.ndarray = np.zeros((3, 3)).astype(int)
+        self.first_turn: int = 1  # X moves first by default
+        self.turn: int = self.first_turn
         self.done: bool = False
-        
+
         if field is not None:
             if len(field) == 9:
-                self.board = np.array(field).astype(int).reshape((3,3))
+                self.board = np.array(field).astype(int).reshape((3, 3))
             else:
                 raise ValueError
-        
+
     def reset(self) -> TicTacToeEnvironment:
         """Resets the board for new game
-        
+
         Returns:
             Environment ready for new game
         """
 
-        self.board = np.zeros((3,3)).astype(int)
-        self.turn = 1 if self.turn == 2 else 2  # We change who moves first in each new game
+        self.board = np.zeros((3, 3)).astype(int)
+        self.first_turn = (
+            1 if self.turn == 2 else 2
+        )  # We change who moves first in each new game
+        self.turn = self.first_turn
         self.done = False
 
         return self
 
-    def check_winner(self) -> Tuple[bool, int | None]:
+    def check_winner(self) -> Tuple[bool, int]:
         """Checks if someone has won
-        
+
         Returns:
-            Tuple of self.done and the winner. If the game is not finished or it's a draw, the winner is None.
+            Tuple of gameover flag and the winner. If the game is not finished or it's a draw, the winner is 0.
+        """
+        for i in range(3):
+            if np.all(self.board[i, :] == 1) or np.all(self.board[:, i] == 1):
+                return (True, 1)
+            if np.all(self.board[i, :] == 2) or np.all(self.board[:, i] == 2):
+                return (True, 2)
+
+        if np.all(np.diag(self.board) == 1) or np.all(np.fliplr(self.board) == 1):
+            return (True, 1)
+        if np.all(np.diag(self.board) == 2) or np.all(np.fliplr(self.board) == 2):
+            return (True, 2)
+
+        if np.sum(self.board) >= 9:
+            return (True, 0)
+
+        return (False, 0)
+
+    def check_appropriate(self, action: Tuple[int, int]) -> bool:
+        """Checks if the move is appropriate
+
+        Args:
+            action: tuple with a move - a row index and a column index.
+
+        Returns:
+            True if the move is allowed, false otherwise
         """
 
-        # somehow efficiently check the win conditions on self.board
+        if self.done:
+            return False
 
-        return (self.done, winner)
+        if action[0] < 0 or action[0] > 2:
+            return False
 
-    def step(self, action) -> Tuple[np.ndarray, None, bool, None]:
+        if action[1] < 0 or action[1] > 2:
+            return False
+
+        if self.board[action[0]][action[1]] != 0:
+            return False
+
+        return True
+
+    def step(self, action: Tuple[int, int]) -> Tuple[np.ndarray, None, bool, int]:
         """Apply player's action
 
         Args:
-            action: action of a player
-        
+            action: action of a player in form of tuple with two indicies(row and column)
+
         Returns:
-            Tuple of (next_state, reward, done, info). next_state is the board state after the move. 
-            reward and info are empty and are present for compatibility with API only. 
+            Tuple of (next_state, reward, done, info). next_state is the board state after the move.
+            reward is empty and is present for compatibility with API only.
             done is a flag if the game has ended.
+            info contains a symbol of winner(0 in case of a draw or running game).
 
         Raises:
             ValueError: if the move is prohibited.
         """
 
-        # call check_winner somewhere here
+        if not self.check_appropriate(action):
+            raise ValueError("The move is inappropriate")
 
-        return (next_state,None,done,None)
+        self.board[action[0]][action[1]] = self.turn
+
+        done: bool = False
+        info: int = 0
+        done, info = self.check_winner()
+
+        self.done = done
+        self.turn = 1 if self.turn == 2 else 2
+
+        return (self.board, None, done, info)
 
     # ToDo
-    def render(self, mode = "gui" | "headless"):
+    def render(self, mode: Literal["gui", "headless"] = "headless") -> None:
         pass
